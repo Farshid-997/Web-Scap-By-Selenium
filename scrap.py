7bcd36c9db704a43bffd530be18a7c32c45288ec
Farshid-997 <arnob.farshid@gmail.com>
2022-08-10 23:08:52 +0600

    csv added

--- scrap.py
+++ scrap.py
@@ -2,22 +2,38 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import csv
+import pandas as pd
+from csv import writer
+
+
+file_name = 'Sample_Data.csv'
+
+column_headings = [
+    'CO2 Emission Saved (ton)', 'Equivalent Trees Plant (trees)', 'Total Income ($)']
+with open(file_name, 'a+', newline='') as write_obj:
+    csv_writer = writer(write_obj)
+    csv_writer.writerow(column_headings)
+
+
+def append_list_as_row(file_name, list_of_elem):
+    with open(file_name, 'a+', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+        csv_writer.writerow(list_of_elem)
+
+
 option = webdriver.ChromeOptions()
-# I use the following options as my machine is a window subsystem linux.
-# I recommend to use the headless option at least, out of the 3
-# option.add_argument('--headless')
+
 option.add_argument('--no-sandbox')
 option.add_argument('--disable-dev-sh-usage')
-# Replace YOUR-PATH-TO-CHROMEDRIVER with your chromedriver location
+
 driver = webdriver.Chrome(
     'C:\Program Files (x86)\WebDriver\chromedriver', options=option)
 
-# Getting page HTML through request
 
 driver.get('https://fop.saj-electric.com/bigScreen/rollSinglePlant?viewKey=363e6363922508f0adb17ecc889b2def1d7e3265472a6ab9fda0a682a0fa9fc7f5d7d20c5c8389c4b2ba38cb3fd9908f8ea64a9c4ec7cb3264a052519ea3a274e4277fe376c2b2a44e24490e53785083eb9077c945b217d991eb6df0b13fa74b&start=3&lang=en')
-time.sleep(5)
+time.sleep(300)
 while(True):
-    time.sleep(5)
+    time.sleep(300)
     url = driver.current_url
 
     driver.get(url)
@@ -25,7 +41,10 @@ while(True):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     links = soup.select(".cumulate li")
 
+    global_list = []
     for i in links:
-        print(i.text)
+        data = str(i.text).split('\n')
+        data_value = data[1]
+        global_list.append(data_value)
 
-    # driver.close()
+    append_list_as_row(file_name, global_list)
